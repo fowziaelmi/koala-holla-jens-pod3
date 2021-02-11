aacfb2ea3071c85002b2b36c16067fd8605b8cc6
Faduma Elmi <fowziiae@gmail.com>
2021-02-11 14:44:16 -0600

    fixed the pool.js

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,9 +1,5 @@
 const pg = require('pg');
 
-
-const pool = new pg.Pool({
-  database: 'koala_holla',
-
 const config = {
   database: 'koala_holla',
   host: 'localhost',
@@ -11,7 +7,6 @@ const config = {
   max: 10,
   idleTimeoutMillis: 30000,
 };
-
 const pool = new pg.Pool(config);
 
 pool.on('connect', () => {
