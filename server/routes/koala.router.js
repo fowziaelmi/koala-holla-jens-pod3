e28230e6b12acf919c038869b0d1763b0d973dee
George Papagapitos <george.g.papagapitos@gmail.com>
2021-02-11 14:07:50 -0600

    added GET endpoint

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const koalaRouter = express.Router();
-const pool = require('../modules/pool');
 
 // DB CONNECTION
-
+const pool = require('../modules/pool');
 
 // GET
-
+koalaRouter.get('/', (req, res) => {
+  pool
+    .query(`SELECT * FROM "koalas"`)
+    .then(function (dbRes) {
+      console.log('dbRes.rows', dbRes.rows);
+      res.send(dbRes.rows);
+    })
+    .catch(function (error) {
+      console.log('GET error', error);
+      res.sendStatus(500);
+    });
+});
 
 // POST
 
-
 // PUT
-
 
 // DELETE
 
