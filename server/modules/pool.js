7fc9f17448747ea8389e831e6c7308a242e6491a
George Papagapitos <george.g.papagapitos@gmail.com>
2021-02-11 16:39:30 -0600

    post + get working

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,26 +1,7 @@
 const pg = require('pg');
 
-
 const pool = new pg.Pool({
   database: 'koala_holla',
-
-const config = {
-  database: 'koala_holla',
-  host: 'localhost',
-  port: 5432,
-  max: 10,
-  idleTimeoutMillis: 30000,
-};
-
-const pool = new pg.Pool(config);
-
-pool.on('connect', () => {
-  console.log('connected to postgres');
-});
-
-pool.on('error', (err) => {
-  console.log('error connecting to postgres', err);
-
 });
 
 module.exports = pool;
